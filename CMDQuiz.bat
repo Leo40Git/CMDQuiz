40858ce821f35fbbcf336aae10f1b43cd8809f84
Kfir Awad <Kfir Awad>
2016-03-01 14:13:06 +0200

    Update 1.0.1 (build 2)

--- CMDQuiz.bat
+++ CMDQuiz.bat
@@ -1,15 +1,18 @@
 @echo off
+set command=nul
+if exist .skipUpdateCheck (set command=goto launch) else (set command=goto checkForUpdates)
 :: Launcher
 :setup
 cls
 set col=9F
 color %col%
-set version=1.0.0
-set build=1
+set version=1.0.1
+set build=2
 title CMDQuiz Version %version% (build %build%)
+%command%
 :checkForUpdates
 if exist version.txt del version.txt
-bitsadmin /transfer checkForUpdates /download /priority normal https://www.dropbox.com/s/oe2k15b58i7hqny/version.txt?dl=1 %CD%\version.txt
+bitsadmin /transfer checkForUpdates /download /priority normal https://www.dropbox.com/s/oe2k15b58i7hqny/version.txt?dl=1 %CD%\version.txt>nul
 set /p newBuild=<version.txt
 if %build% LSS %newBuild% goto update_newVersion
 if %build% GEQ %newBuild% goto update_upToDate
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CMDQuiz
 A quiz game being made in native Batch script.
+
+# To-do list
+
+- [ ] ADD MORE LEVELS!
+- [ ] Finish instruction & about sections
+
+## Extra stuff
+
+- You can skip the update check by adding a file called ".skipUpdateCheck" to the root directory.
